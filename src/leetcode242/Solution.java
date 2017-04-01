0d560e4df70083203d3199be6586a1b163baa603
Lb <li.biao@whu.edu.cn>
2017-04-01 18:07:23 +0800

    s

--- src/leetcode242/Solution.java
+++ src/leetcode242/Solution.java
@@ -4,4 +4,10 @@ package leetcode242;
  * Created by Lb on 17-4-1.
  */
 public class Solution {
+    public boolean isAnagram(String s, String t){
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
--- src/leetcode504/Solution.java
+++ src/leetcode504/Solution.java
@@ -2,6 +2,13 @@ package leetcode504;
 
 /**
  * Created by Lb on 17-4-1.
+ *
+ Given two strings s and t, write a function to determine if t is an anagram of s.
+
+ For example,
+ s = "anagram", t = "nagaram", return true.
+ s = "rat", t = "car", return false.
  */
 public class Solution {
+
 }
