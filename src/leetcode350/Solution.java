130a2f929a949393865ba7ea14748df1e25ac0e4
lbsswhu <biao.li@whu.edu.cn>
2017-04-03 16:19:51 +0800

    350 两个集合的交集 HashMap 和 HashSet 是 Java Collection Framework 的两个重要成员，其中 HashMap 是 Map 接口的常用实现类，HashSet 是 Set 接口的常用实现类。虽然 HashMap 和 HashSet 实现的接口规范不同，但它们底层的 Hash 存储机制完全一样，甚至 HashSet 本身就采用 HashMap 来实现的。

---
+++ src/leetcode350/Solution.java
@@ -0,0 +1,67 @@
+package leetcode350;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Lb on 17-4-3.
+ * Given two arrays, write a function to compute their intersection.
+
+ Example:
+ Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+ */
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                res.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] > nums2[j])
+                j++;
+            else
+                i++;
+        }
+        int[] r = new int[res.size()];
+        for (int k = 0; k < res.size(); k++) {
+            r[k] = res.get(k);
+        }
+        return r;
+    }
+}
+
+/*
+//使用hashMap
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i = 0; i < nums1.length; i++)
+        {
+            if(map.containsKey(nums1[i])) map.put(nums1[i], map.get(nums1[i])+1);
+            else map.put(nums1[i], 1);
+        }
+
+        for(int i = 0; i < nums2.length; i++)
+        {
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0)
+            {
+                result.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i])-1);
+            }
+        }
+
+       int[] r = new int[result.size()];
+       for(int i = 0; i < result.size(); i++)
+       {
+           r[i] = result.get(i);
+       }
+
+       return r;
+    }
+}
+ */
