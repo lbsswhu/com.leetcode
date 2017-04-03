8cd7419240f68ad3587847b7757ae96a78d37f62
lbsswhu <biao.li@whu.edu.cn>
2017-04-03 17:20:47 +0800

    Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.

--- src/leetcode268/Solution.java
+++ src/leetcode268/Solution.java
@@ -22,8 +22,8 @@ public int missingNumber(int[] nums) { //xor
     }
     return res;
 }
-
 Binary Search
+
 public int missingNumber(int[] nums) { //binary search
     Arrays.sort(nums);
     int left = 0, right = nums.length, mid= (left + right)/2;
