6f9e5a9eb5890ef8ea6095998fb4ceca5bdf8267
lbsswhu <biao.li@whu.edu.cn>
2017-05-02 22:21:05 +0800

    415. Add Strings

---
+++ src/leetcode415/Solution.java
@@ -0,0 +1,17 @@
+package leetcode415;
+
+/**
+ * Created by Lb on 17-5-2.
+ */
+public class Solution {
+    public String addStrings(String num1, String num2) {
+        String res = null;
+        int count =0;
+        String nums1 = new StringBuilder(num1).reverse().toString();
+        String nums2 = new StringBuilder(num2).reverse().toString();
+        for (int i = 0; i < Math.max(num1.length(),num2.length()); i++) {
+
+        }
+        return res;
+    }
+}
