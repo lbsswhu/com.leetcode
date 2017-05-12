68d4e743ca9995db41f2de808bf992cbc2b45eca
Lb <li.biao@whu.edu.cn>
2017-05-12 17:31:33 +0800

    Power of Three

---
+++ src/leetcode326/Solution.java
@@ -0,0 +1,10 @@
+package leetcode326;
+
+/**
+ * Created by Lb on 17-5-12.
+ */
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        return ( n>0 &&  1162261467%n==0);
+    }
+}
