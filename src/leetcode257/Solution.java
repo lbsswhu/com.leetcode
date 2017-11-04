fef3655f15ec1f2531da949f6e6c32413007273a
lbsswhu <biao.li@whu.edu.cn>
2017-11-04 16:33:06 +0800

    Binary Tree Paths 遍历叶子结点的路径

--- src/leetcode257/Solution.java
+++ src/leetcode257/Solution.java
@@ -1,4 +1,36 @@
 package leetcode257;
+import java.util.ArrayList;
+import java.util.List;
 
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
 public class Solution {
+    public List<String> binaryTreePaths(TreeNode root){
+        List<String> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        if(root.left != null){
+            List<String> leftpath = binaryTreePaths(root.left);
+            for (String path:
+                 leftpath) {
+                result.add(""+root.val+"->"+path);
+            }
+        }
+        if(root.right != null){
+            List<String> rightpath = binaryTreePaths(root.right);
+            for (String path:
+                    rightpath) {
+                result.add(""+root.val+"->"+path);
+            }
+        }
+
+        if(root.left==null && root.right==null){
+                result.add(String.valueOf(root.val));
+        }
+        return result;
+    }
 }
