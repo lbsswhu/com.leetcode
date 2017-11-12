18be2aa4d18bb759e384013280796eaa4e4691cd
lbsswhu <biao.li@whu.edu.cn>
2017-11-12 11:56:46 +0800

    Repeated Substring Pattern 判断一个子串是否能由其一个子串重复若干次组成

--- src/leetcode459/Solution.java
+++ src/leetcode459/Solution.java
@@ -33,37 +33,33 @@ public class Solution {
     //kmp算法
     public boolean repeatedSubstringPattern(String str) {
         //This is the kmp issue
-        int[] prefix = kmp(str);
-        int len = prefix[str.length()-1];
-        int n = str.length();
-        return (len > 0 && n%(n-len) == 0);
-    }
-    private int[] kmp(String s){
-        int len = s.length();
-        int[] res = new int[len];
-        char[] ch = s.toCharArray();
-        int i = 0, j = 1;
-        res[0] = 0;
-        while(i < ch.length && j < ch.length){
-            if(ch[j] == ch[i]){
-                res[j] = i+1;
+        int i=0,j=1;
+        char[] s = str.toCharArray();
+        int[] next = new int[s.length];
+        next[0]=0;
+        while(i<s.length && j<s.length){
+            if(s[i]==s[j]){
+                next[j]=i+1;
                 i++;
                 j++;
             }else{
-                if(i == 0){
-                    res[j] = 0;
+                if(i==0){
+                    next[j]=0;
                     j++;
                 }else{
-                    i = res[i-1];
+                    i= next[i-1];
                 }
             }
         }
-        return res;
+        if(next[s.length-1]>0 && s.length%(s.length-next[s.length-1])==0)
+            return true;
+        else
+            return false;
     }
 
     public static void main(String[] args) {
         Solution test = new Solution();
-        if(test.repeatedSubstringPattern("abacababacab")==true)
+        if(test.repeatedSubstringPattern("ababa")==true)
             System.out.println("true");
         else
             System.out.println("false");
