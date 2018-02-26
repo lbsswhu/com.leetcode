23e7aad3b86214401358c5a3ffd90d2c4f4ce7ab
lbsswhu <biao.li@whu.edu.cn>
2018-02-26 11:34:45 +0800

    434. Number of Segments in a String

---
+++ src/leetcode434/Solution.java
@@ -0,0 +1,17 @@
+package leetcode434;
+
+/**
+ * @author lbsswhu
+ * @create 2018 02 26 11:02
+ * @Description
+ **/
+public class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+        for(int i =0;i<s.length();i++) {
+            if((i==0 || s.charAt(i-1)== ' ')&&s.charAt(i) != ' ')
+                count++;
+        }
+        return count;
+    }
+}
